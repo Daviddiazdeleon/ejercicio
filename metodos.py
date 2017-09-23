3d1daf779b212daad026569e26b9133375f70cd6
javier <correodejavier@gmail.com>
2017-09-22 21:28:39 -0500

    funcional

--- metodos.py
+++ metodos.py
@@ -14,30 +14,11 @@ class Producto:
                                   )
 
 
-    def __getitem__(self, key):
-        return self.nombre[key]
-
-
-def compara( x, y ) :
-
-    if self.Name < other.Name :
-      rst = -1
-    elif self.Name > other.Name :
-      rst = 1
-    else :
-      rst = 0
-
-    return rst
-
-prod = [auto, compu, cel]
-prodnom = prod.sort( compara )
-
-print prodnom
-
-
 auto = Producto('f500', 10000, 'rojo', 'ferrari')
 compu = Producto('v1000', 1500 , 'negra', 'dell')
 cel = Producto ('mate 8', 1900 , 'blanco', 'huawey')
 auto2 = Producto ('ford', 12000 , 'plata', 'fiest')
 objetos = (auto, compu, cel, auto2)
+
 print sorted(objetos, key=lambda producto: producto.nombre)
+print sorted(objetos, key=lambda producto: producto.precio)
