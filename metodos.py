455217fd490d7e60edf31225d0aa756072e0bb72
Daviddiazdeleon <32183776+Daviddiazdeleon@users.noreply.github.com>
2017-09-22 20:31:06 -0500

    metodos

---
+++ metodos.py
@@ -0,0 +1,21 @@
+import Producto
+
+auto = Producto()
+auto.nombre = "f500"
+auto.precio = 10000000
+auto.color = "rojo"
+auto.marca = "ferrari"
+
+compu = Producto()
+compu.nombre = "v1000"
+compu.precio = 15000
+compu.color = "negra"
+compu.marca = "dell"
+
+cel = Producto()
+cel.nombre = "mate 8"
+cel.precio = 8000
+cel.color = "blanco"
+cel.marca = "huawei"
+
+
